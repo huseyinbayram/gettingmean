0cf2565e64fd8275770240673176b2e7c2093fcf
kozmobot <kozmobot@gmail.com>
2016-05-06 19:19:57 +0300

    Added Passport

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -7,9 +7,66 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
-    password: String
+    email: {
+        type: String,
+        index: true,
+        match: /.+\@.+\..+/
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        validate: [
+            function (password) {
+                return password.length >= 6;
+            },
+            'Password should be longer'
+        ]
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    website: {
+        type: String,
+        get: function (url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url;
+                }
+                return url;
+            }
+        }
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
+    }
 });
+
+UserSchema.statics.findOneByUsername = function (username, callback) {
+    this.findOne({ username: new RegExp(username, 'i') }, callback);
+};
+
+UserSchema.methods.authenticate = function (password) {
+    return this.password === password;
+}
+
+UserSchema.virtual('fullName').get(function() {
+    return this.firstName + ' ' + this.lastName;
+}).set(function (fullName) {
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+
+UserSchema.set('toJSON', { getters: true, virtuals: true});
 
 mongoose.model('User', UserSchema);
